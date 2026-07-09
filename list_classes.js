e527896ed3c526baa8cc712a8c71523ce0619e3a
Aga-42i <rahim.bahsiyev@gmail.com>
2026-07-09 13:29:43 +0400

    Removing Left-over pieces

--- list_classes.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('C:\\Users\\ASUS\\Desktop\\trevaa\\treva-next\\apps\\web\\app\\page.tsx', 'utf8');
-const matches = content.match(/section_[a-zA-Z0-9_-]+/g);
-console.log([...new Set(matches)]);
